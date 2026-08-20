554408a000e1ee7832cf37af0e8fd2ad144f4ad6
Greg Drake <greg@madmallards.com>
2026-08-20 00:12:25 -0400

    Match Gitleaks module path

--- tests/test_full_repository_container.py
+++ tests/test_full_repository_container.py
@@ -8,7 +8,7 @@ def test_container_bundles_full_repository_engines() -> None:
 
     assert "semgrep==1.172.0" in dockerfile
     assert "checkov==3.3.8" in dockerfile
-    assert "github.com/gitleaks/gitleaks/v8@v8.30.1" in dockerfile
+    assert "github.com/zricethezav/gitleaks/v8@v8.30.1" in dockerfile
     assert "semgrep --version" in dockerfile
     assert "gitleaks version" in dockerfile
     assert "checkov --version" in dockerfile
