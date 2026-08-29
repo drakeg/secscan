8f1820bc437911bf815ab9c71dee6114ee5f855c
Greg Drake <greg@madmallards.com>
2026-08-28 22:59:15 -0400

    Sprint 58: verify ECS association packaging

--- scripts/verify_wheel.py
+++ scripts/verify_wheel.py
@@ -9,6 +9,8 @@ REQUIRED_FILES = {
     "secscan/assets.py",
     "secscan/assets_web.py",
     "secscan/aws.py",
+    "secscan/aws_ecs.py",
+    "secscan/aws_ecs_cli.py",
     "secscan/cli.py",
     "secscan/compare.py",
     "secscan/epss.py",
