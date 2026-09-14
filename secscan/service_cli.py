759f2d9146b3f076a58c0f9da4269b86b06646ea
Greg Drake <greg@madmallards.com>
2026-09-13 21:29:15 -0400

    Sprint 70: mount project API

--- secscan/service_cli.py
+++ secscan/service_cli.py
@@ -36,6 +36,7 @@ def main() -> None:
     from secscan.auth import mount_auth
     from secscan.credential_tenancy import SshCredentialTenantMiddleware
     from secscan.network_range_web import mount_network_range_submission
+    from secscan.projects import mount_projects
     from secscan.public_navigation import PublicSessionNavigationMiddleware
     from secscan.public_site import mount_public_site
     from secscan.service import create_app
@@ -60,6 +61,7 @@ def main() -> None:
         mount_public_site(app, database=database)
         mount_auth(app, database=database, api_token=api_token)
         mount_tenant_invitations(app, database=database)
+        mount_projects(app, database=database)
         mount_ssh_host_trust(app, database=database)
         mount_assets(app, database=database)
         mount_network_range_submission(app)
